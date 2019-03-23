7165fbbdaabcbebb486711bcc6c7ef1e8bc275e8
Erik Faye-Lund <erik.faye-lund@collabora.com>
2019-03-23 12:28:36 +0100

    font-awesome: use CDN version instead of hosting a copy
    The tags were copied from here, because font-awesome 4.x requires
    registration to use their official CDN:
    
    https://www.bootstrapcdn.com/fontawesome/

--- copy-assets.bash
+++ copy-assets.bash
@@ -6,7 +6,5 @@ if [ $# -ne 1 ]; then
 fi
 
 cp -r media $1/ &&
-cp -r assets/font-awesome-4.7.0/fonts/. $1/ &&
-cp assets/font-awesome-4.7.0/css/font-awesome.min.css $1/font-awesome.css &&
 cp assets/prism/prism.{css,js} $1/ &&
 cp assets/site.{css,js} $1/
