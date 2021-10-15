d7573495504efdcd63a33fedd80577f83cbd19ac
Morten Daniel Fornes <mdfornes@gmail.com>
2021-10-15 20:41:35 +0700

    extract build.bash from .travis.yml

---
+++ build.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+dotnet run -p generator/src/generator.csproj -- . "https://fuseopen.com/docs/" _site
+./copy-assets.bash _site
+find _site/media -type f \( -iname "*.png" -or -iname "*.jpg" \) -exec mogrify -strip -resize 450x450\> {} \;
+find _site/media -type f -iname "*.png" -exec pngquant {} \; -exec optipng -silent {} \;
+touch _site/.nojekyll
