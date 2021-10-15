5d45b984576486fb840b40a822f5f05c5ead423d
Morten Daniel Fornes <mdfornes@gmail.com>
2021-10-15 21:06:13 +0700

    increase image sizes

--- build.bash
+++ build.bash
@@ -2,6 +2,6 @@
 set -e
 dotnet run -p generator/src/generator.csproj -- . "https://fuseopen.com/docs/" _site
 ./copy-assets.bash _site
-find _site/media -type f \( -iname "*.png" -or -iname "*.jpg" \) -exec mogrify -strip -resize 450x450\> {} \;
+find _site/media -type f \( -iname "*.png" -or -iname "*.jpg" \) -exec mogrify -strip -resize 850x850\> {} \;
 find _site/media -type f -iname "*.png" -exec pngquant {} \; -exec optipng -silent {} \;
 touch _site/.nojekyll
