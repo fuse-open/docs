1b869754fdd8cea1d72e3c10f330291ac45533a3
Morten Daniel Fornes <mdfornes@gmail.com>
2021-10-17 13:01:58 +0700

    Tooling enhancements
    This adds package.json with scripts to make it easier to work on this
    project locally. For example:
    
        npm install
        npm start
    
    This will build HTML pages and start a webserver at http://localhost:8080/
    
    Also adds some more scripts and options for advanced usage.

--- build.bash
+++ build.bash
@@ -1,7 +1,31 @@
 #!/bin/bash
+SELF=`echo $0 | sed 's/\\\\/\\//g'`
+cd "`dirname "$SELF"`" || exit 1
 set -e
-dotnet run -p generator/src/generator.csproj -- . "https://fuseopen.com/docs/" _site
-./copy-assets.bash _site
-find _site/media -type f \( -iname "*.png" -or -iname "*.jpg" \) -exec mogrify -strip -resize 850x850\> {} \;
-find _site/media -type f -iname "*.png" -exec pngquant {} \; -exec optipng -silent {} \;
-touch _site/.nojekyll
+
+URL="https://fuseopen.com/docs/"
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+    -l|--local)
+        shift
+        URL="http://localhost:8080/"
+        ;;
+    *)
+        break
+        ;;
+    esac
+done
+
+echo "URL: $URL (Please wait for docs to finish building...)"
+
+if [ "$BUILD" != 0 ]; then
+    dotnet run -p generator/src/generator.csproj -- . "$URL" _site
+fi
+
+if [ "$ASSETS" != 0 ]; then
+    ./copy-assets.bash _site
+    find _site/media -type f \( -iname "*.png" -or -iname "*.jpg" \) -exec mogrify -strip -resize 850x850\> {} \;
+    find _site/media -type f -iname "*.png" -exec pngquant {} \; -exec optipng -silent {} \;
+    touch _site/.nojekyll
+fi
